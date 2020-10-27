5c5a12fa759e556eede83ba2c9a07e58a675f503
Zuhair Bennasser <eng.bennasser@gmail.com>
2020-10-27 14:34:48 -0400

    commit

--- jobs/app.py
+++ jobs/app.py
@@ -1,5 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, g
+import sqlite3
+PATH ="db/jobs.sqlite"
 app= Flask(__name__)
+
+def open_connection:
+    connection = getattr(g= None)
 
 @app.route("/")
 @app.route("/jobs")
