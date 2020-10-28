de660c4d9b9e165be0c1d43c7334fcf0398f668c
Zuhair Bennasser <eng.bennasser@gmail.com>
2020-10-28 16:36:21 -0400

    commit

--- jobs/app.py
+++ jobs/app.py
@@ -29,13 +29,14 @@ def close_connection(exception):
     connection = getattr(g, '_connection', None)
     if connection is not None:
         connection.close()
-           
+
 
 @app.route("/")
 @app.route("/jobs")
  
 def jobs():
-    return render_template('index.html')
+    jobs= execute_sql('SELECT job.id, job.title, job.description, job.salary, employer.id as employer_id, employer.name as employer_name FROM job JOIN employer ON employer.id = job.employer_id')
+    return render_template('index.html', jobs = jobs)
 
 
  
