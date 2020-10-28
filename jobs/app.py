de9d05ea28d1744d66ceeaff18d7e2648ebd4f89
Zuhair Bennasser <eng.bennasser@gmail.com>
2020-10-28 14:59:31 -0400

    commit

--- jobs/app.py
+++ jobs/app.py
@@ -3,8 +3,24 @@ import sqlite3
 PATH ="db/jobs.sqlite"
 app= Flask(__name__)
 
-def open_connection:
-    connection = getattr(g= None)
+def open_connection():
+    connection = g.getattr()
+
+    if connection== None:
+        connection=sqlite3.connect("db/jobs.sqlite")
+
+    row_factory = sqlite3.Row
+
+    
+
+    def execute_sql(sql, values=(), commit=False, single= False):
+        connection=connection
+        cursor=connection.execute_sql()
+
+
+
+
+
 
 @app.route("/")
 @app.route("/jobs")
