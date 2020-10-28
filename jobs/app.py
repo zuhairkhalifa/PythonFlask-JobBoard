b0d23fdd0abd0f600343fd2ff5c123fb45132f62
Zuhair Bennasser <eng.bennasser@gmail.com>
2020-10-28 15:18:28 -0400

    commit

--- jobs/app.py
+++ jobs/app.py
@@ -1,15 +1,30 @@
 from flask import Flask, render_template, g
 import sqlite3
-PATH ="db/jobs.sqlite"
+PATH ='db/jobs.sqlite'
 app= Flask(__name__)
 
 def open_connection():
-    connection = g.getattr()
+    connection = getattr(g, '_connection',None)
 
     if connection== None:
-        connection=sqlite3.connect("db/jobs.sqlite")
+        connection=g._connection= sqlite3.connect(PATH)
 
-    row_factory = sqlite3.Row
+    connection.row_factory = sqlite3.Row
+    return connection
+
+
+def execute_sql(sql, values=(), commit=False, single = False ):
+    connection = open_connection()
+    cursor = connection.execute(sql, values)
+    if commit == True:
+        results = connection.commit()
+    else:
+        results= cursor.fetchone() if single else cursor.fetchall()
+    cursor.close()
+    return results
+
+
+@app.teardown_appcontext    
 
     
 
