be111b6100196dc5c8636ec30becdc490e5ee056
Zuhair Bennasser <eng.bennasser@gmail.com>
2020-10-29 21:25:40 -0400

    commit

--- jobs/app.py
+++ jobs/app.py
@@ -48,7 +48,7 @@ def job(job_id):
 
 @app.route('/employer/<employer_id>')
 def employer(employer_id):
-    employer= execute_sql('SELECT * from employer WHERE employer.id=?', [employer_id], single= True)
+    employer= execute_sql('SELECT * FROM employer WHERE id=?', [employer_id], single= True)
     jobs= execute_sql('SELECT job.id, job.title, job.description, job.salary FROM job JOIN employer ON employer.id = job.employer_id WHERE employer.id = ?',[employer_id])
     reviews= execute_sql('SELECT review, rating, title, date, status FROM review JOIN employer ON employer.id = review.employer_id WHERE employer.id = ?', [employer_id])
     return render_template('employer.html', employer = employer, jobs = jobs, reviews =reviews)
